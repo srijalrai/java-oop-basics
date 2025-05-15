61410a0c490d8a96658bec9d3e0e7b497e7467f3
srijalrai <raisrijal051103@gmail.com>
2025-05-15 23:09:57 +0530

    Add Person class

---
+++ src/com/packages/Main.java
@@ -0,0 +1,10 @@
+package com.packages;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //write your code here
+        Person p = new Person("Srijal", 20);
+        p.greet();
+    }
+}
---
+++ src/com/packages/Person.java
@@ -0,0 +1,16 @@
+package com.packages;
+
+public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public void greet(){
+        System.out.println("Hello my name is " + name + ", and I am " + age + " years old.");
+    }
+
+}
